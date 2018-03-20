a3501aa5cab34391a069088bae445bc4bbf3adad
Brad Flood <bflood@keyholesoftware.com>
2018-03-20 18:20:25 -0500

    move EJB2 descriptor

--- src/main/java/com/keyholesoftware/refactor/descriptor/model/EnterpriseBean.java
+++
@@ -1,9 +0,0 @@
-package com.keyholesoftware.refactor.descriptor.model;
-
-import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
-
-public abstract class EnterpriseBean {
-    @JacksonXmlProperty(localName = "ejb-name")
-    private String ejbName;
-        
-}
--- src/test/java/com/keyholesoftware/refactor/descriptor/model/StatelessSessionDescriptorParserTest.java
+++ src/test/java/com/keyholesoftware/refactor/descriptor/model/StatelessSessionDescriptorParserTest.java
@@ -1,11 +1,13 @@
-package com.keyholesoftware.refactor.descriptor.model.ejb2;
+package com.keyholesoftware.refactor.descriptor.model;
 
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import com.keyholesoftware.refactor.descriptor.model.ejb2.EjbJar;
+import com.keyholesoftware.refactor.descriptor.model.ejb2.EjbLocalRefBuilder;
 import com.keyholesoftware.refactor.descriptor.model.ejb2.Session;
+import com.keyholesoftware.refactor.descriptor.model.ejb2.SessionBuilder;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
